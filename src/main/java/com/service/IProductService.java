6d0ea5730737bbb2afa201216c5ba89b76400e34
Thang0226 <thang.nd0226@gmail.com>
2024-12-27 14:04:03 +0700

    first commit

---
+++ src/main/java/com/service/IProductService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import com.model.Product;
+
+import java.util.Optional;
+
+public interface IProductService {
+    Iterable<Product> findAll();
+
+    Optional<Product> findById(Long id);
+}
