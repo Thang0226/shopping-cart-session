025beaee6de8c3dfac1c22c8ed6b3d7570b62313
Thang0226 <thang.nd0226@gmail.com>
2024-12-27 16:05:38 +0700

    can view products

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -5,8 +5,10 @@ import com.model.Product;
 import com.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -22,9 +24,16 @@ public class ProductController {
     }
 
     @GetMapping("/shop")
-    public ModelAndView showShop() {
-        ModelAndView modelAndView = new ModelAndView("shop");
-        modelAndView.addObject("products", productService.findAll());
+    public String showShop(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "shop";
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView showView(@PathVariable Long id) {
+        Optional<Product> product = productService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("view_product");
+        modelAndView.addObject("product", product.get());
         return modelAndView;
     }
 
